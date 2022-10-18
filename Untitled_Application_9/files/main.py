8d5b016358cef5763bf24124dc810bcaaf057320
Baseten <integration@baseten.co>
2022-10-18 20:49:15 +0000

    publish changes

---
+++ Untitled_Application_9/files/main.py
@@ -0,0 +1,21 @@
+# Functions that are linked to a worklet code block require a unique signature. In order to connect a worklet
+# code block to a Python function the function has to use the arguments block_input, env, and context and
+# return a JSON-serializable which becomes the input for the next block in the worklet.
+
+# Learn more at https://docs.baseten.co/applications/worklets
+
+# Here's an example of a simple passthrough using the required signature:
+
+def passthrough(block_input, env, context):
+    """
+    Args:
+      block_input (JSON-serializable): The input to this block.
+      env (dict): A mutable dictionary that holds its state as
+        it traverses the blocks of a worklet.
+      context (Context): Provides helper methods for data access.
+        See: https://docs.baseten.co/applications/worklets#context
+
+    Returns:
+      A JSON-serializable, which becomes the input to the next block in the worklet.
+    """
+    return block_input
